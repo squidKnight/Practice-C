bdec371b9a80ea498f017405a105a62908866cd2
squidKnight <33043582+squidKnight@users.noreply.github.com>
2017-10-23 22:04:03 -0400

    Added some parameters noted in README
     Simple additions. I'm not 100% how each part of the code is intended to be used with the state machine, so I made my best guesses. I'm guessing that line_runBehavior is where the actual line follow code is supposed to go. I didn't edit any of the headers to adapt the parameter changes, in case this kind of approach isn't what we're looking for.

--- Practice C/src/lineFollowBehavior.c
+++ Practice C/src/lineFollowBehavior.c
@@ -1,4 +1,5 @@
 #include "lineFollowBehavior.h"
+#include <kipr/botball.h>
 
 typedef struct line_vtbl line_vtbl;
 struct line_vtbl {
@@ -9,10 +10,26 @@ static line_vtbl the_line_vtbl = {
   line_runBehavior
 };
 
-void line_runBehavior(line *l)
-{
-  
-}
+void line_runBehavior(line *l/*what's this used for?*/, time, leftPort, rightPort, sensorMain, sensorSecondary)
+{// Where does the state machine part go??
+  int init = seconds();
+  int watch = seconds() - init;
+  if(sensorSecondary=!-1)//variable set to -1 if there is only one tophat; could also make a new function without second sesnor input
+  {
+    while(watch<time)
+    {
+      /*double-tophat line-follow code here??*/
+      watch = seconds() - init;
+    }
+  }
+  else
+  {
+    while(watch<time)
+    {
+      /*single-tophat line-follow code here??*/
+      watch = seconds() - init;
+    }
+  }
 void line_construct(line *l)
 {
  behavior_construct(&l->base);
